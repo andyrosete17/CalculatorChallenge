1306231628503f70da5819f5b7358a1914d219fb
Andy Rosete <andy.rosete@innovation.group>
2019-04-27 21:57:13 +0200

    Added Calculation Domain

---
+++ Calculator.Domain1/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator.Domain
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Calculator
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstOperand { get; set; }
+
+        public string SecondOperand { get; set; }
+
+        public string Operation { get; set; }
+
+        public string Result { get; private set; }
+
+    }
+}
---
+++ Calculator.Model/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculator.Model
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Calculator
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstOperand { get; set; }
+
+        public string SecondOperand { get; set; }
+
+        public string Operation { get; set; }
+
+        public string Result { get; private set; }
+
+    }
+}
